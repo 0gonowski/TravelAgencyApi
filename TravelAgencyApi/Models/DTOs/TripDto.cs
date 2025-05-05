6c52d279a4dd76c4cf4b25deb3c96adb95511253
s30688 <s30688@pjwstk.edu.pl>
2025-05-05 23:46:51 +0200

    Add project files (cleaned according to gitignore)

---
+++ TravelAgencyApi/Models/DTOs/TripDto.cs
@@ -0,0 +1,12 @@
+﻿namespace TravelAgencyApi.Models.DTOs;
+
+public class TripDto
+{
+    public int IdTrip { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public DateTime DateFrom { get; set; }
+    public DateTime DateTo { get; set; }
+    public int MaxPeople { get; set; }
+    public List<CountryDto> Countries { get; set; } = new List<CountryDto>();
+}
